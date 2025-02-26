2b9202a0e08fc5cfe64c2e3cad79c363399db8ab
Haley Goubert <hmgoubert@gmail.com>
2025-02-25 21:54:49 -0500

    Updates

---
+++ precache-manifest.d90936794f108fd1553740a397fb6104.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac192332071d7408d7fc2c61a2515bd",
+    "url": "/updated-exercise-app/index.html"
+  },
+  {
+    "revision": "ddb36830d64d49ad9ffb",
+    "url": "/updated-exercise-app/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "876413f403a3326c89fd",
+    "url": "/updated-exercise-app/static/js/2.d55b193b.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/updated-exercise-app/static/js/2.d55b193b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88ac8b05bd82ee5d0dbb",
+    "url": "/updated-exercise-app/static/js/3.a6d72d1a.chunk.js"
+  },
+  {
+    "revision": "ddb36830d64d49ad9ffb",
+    "url": "/updated-exercise-app/static/js/main.89e0cc7e.chunk.js"
+  },
+  {
+    "revision": "a290576fe19b076207dd",
+    "url": "/updated-exercise-app/static/js/runtime-main.a8296e1a.js"
+  }
+]);
